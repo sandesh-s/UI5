bbfd37a4b31cc66ecb2724322058781716a403a7
P1941730335 <sandesh.satpute@gmail.com>
2018-12-28 00:20:47 +0000

    Conroller with toast message, but code not working

--- HelloWorldv2/webapp/controller/App.controller.js
+++ HelloWorldv2/webapp/controller/App.controller.js
@@ -1,10 +1,12 @@
 sap.ui.define([
-	"sap/ui/core/mvc/Controller"
-], function (Conrtoller) {
+	"sap/ui/core/mvc/Controller",
+	"sap/m/MessageToast"
+], function (Controller, MessageToast) {
 	"use strict";
    return Controller.extend("sandesh.controller.App", {
 		onShowHellow : function(){
-			alert("Hellow World");
+			//alert("Hello World"); // eslint-disable-line no-alert
+			MessageToast.show("Toast Message");
 		}	
 	});
 });
