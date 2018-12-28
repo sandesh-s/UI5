e66c47f8141d6760c88d418a947b66a93b63ee04
P1941730335 <sandesh.satpute@gmail.com>
2018-12-28 06:39:22 +0000

    Controller now working

--- HelloWorldv2/webapp/controller/App.controller.js
+++ HelloWorldv2/webapp/controller/App.controller.js
@@ -1,9 +1,24 @@
 sap.ui.define([
 	"sap/ui/core/mvc/Controller",
-	"sap/m/MessageToast"
-], function (Controller, MessageToast) {
+	"sap/m/MessageToast",
+	"sap/ui/model/json/JSONModel"
+], function (Controller, MessageToast, JSONModel) {
 	"use strict";
    return Controller.extend("sandesh.controller.App", {
+		//Code to 
+		onInit : function () {
+			// set data model on view
+			var oData = {
+				recipient : {
+					name: "World"
+				}
+			};
+			var oModel = new JSONModel(oData);
+			this.getView().setModel(oModel);
+		},
+		
+		
+		//Code to show toast message
 		onShowHellow : function(){
 			//alert("JS Alert - Hello World"); // eslint-disable-line no-alert
 			MessageToast.show("Toast Message");
