8649d28027ee2e859707d01d26c61d6aa8697a6e
P1941730335 <sandesh.satpute@gmail.com>
2018-12-29 04:06:44 +0000

    i18n Code working

--- HelloWorldv2/webapp/controller/App.controller.js
+++ HelloWorldv2/webapp/controller/App.controller.js
@@ -6,34 +6,29 @@ sap.ui.define([
 ], function (Controller, MessageToast, JSONModel, ResourceModel) {
 	"use strict";
    return Controller.extend("sandesh.controller.App", {
-		//Code to 
 		onInit : function () {
 			// set data model on view
 			var oData = {
 				recipient : {
-					name: "Sample Text"
+					name: "World"
 				}
 			};
 			var oModel = new JSONModel(oData);
 			this.getView().setModel(oModel);
-			
-			//set il8n model on the view more detail here: https://tinyurl.com/y9436r5b
-			var il8nModel = new ResourceModel({
-				bundleName: "sandesh.il8n.il8n"
+			//set i18n model on the view more detail here: https://tinyurl.com/y9436r5b
+			var i18nModel = new ResourceModel({
+				bundleName: "sandesh.i18n.i18n"
 			});
-			this.getView().setModel(il8nModel, "il8n");
-			
+			this.getView().setModel(i18nModel, "i18n");
 		},
-		
-		
 		//Code to show toast message
 		onShowHellow : function(){
-			//Read msg from il8n model more detail here: https://tinyurl.com/y9436r5b
-			var oBundle = this.getView().getModel("il8n").getResourceBundle();
+			//Read msg from i18n model more detail here: https://tinyurl.com/y9436r5b
+			var oBundle = this.getView().getModel("i18n").getResourceBundle();
 			var sRecipient = this.getView().getModel().getProperty("/recipient/name");
 			var sMsg = oBundle.getText("helloMsg", [sRecipient]);
 			//alert("JS Alert - Hello World"); // eslint-disable-line no-alert
-			//MessageToast.show("Toast Message"); //commenting this line since message will be displayed by il8n properties
+			//MessageToast.show("Toast Message"); //commenting this line since message will be displayed by i18n properties
 			MessageToast.show(sMsg);
 		}	
 	});
